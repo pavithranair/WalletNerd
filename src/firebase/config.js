f87c8861e5b9ee82b6eca74a44135224a6b650db
Pavithra Nair <pmpavithranair@gmail.com>
2021-12-08 21:34:15 +0530

    added Authentication

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBBkclQV-G3eFFSaCs4j2nFfDR0MkHCOOs",
+  authDomain: "walletnerd.firebaseapp.com",
+  projectId: "walletnerd",
+  storageBucket: "walletnerd.appspot.com",
+  messagingSenderId: "567609347025",
+  appId: "1:567609347025:web:25df58718d9248dd59aef0"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectFireStore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+export { projectFireStore, projectAuth }
